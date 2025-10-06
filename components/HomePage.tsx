b512b3080687ea5d5c1070ffae1f4bafffeefb2b
banucmsc-commit <banumsc@gmail.com>
2025-10-06 08:42:06 +0530

    fixed the blog and video

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import SEO from './SEO';
 import ContactPopup from './ContactPopup';
-import BannerCarousel from './Bannercarousel';
+import BannerCarousel from './BannerCarousel';
 
 
 
